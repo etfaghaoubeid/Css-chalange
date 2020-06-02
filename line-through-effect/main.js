ee07e9de6ae6de7dd8545e19969c3adf39f8a216
etfagha oubeid <etfagha@softprofiles.com>
2020-06-02 19:11:07 +0000

    it works

--- line-through-effect/main.js
+++ line-through-effect/main.js
@@ -1,2 +1,15 @@
-const element = document.getElementById("text");
-console.log(element)
+const text = document.getElementById('text');
+const textArr = text.innerText.split('');
+
+const newEl = document.createElement('h1');
+newEl.innerHTML = `
+	${textArr.map(letter => `<span class="letter" style="${randomVisibility()}">${letter}</span>`).join('')}
+`;
+newEl.classList.add('overlay');
+
+document.body.appendChild(newEl);
+
+
+function randomVisibility() {
+	return Math.random() < 0.5 ? 'visibility: hidden' : 'visibility: visible';
+}
